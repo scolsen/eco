0fa6fae82e3b252aac2e44ee3833cc29785c70b3
Scott Olsen <scg.olsen@gmail.com>
2018-09-09 23:39:04 -0400

    Move configuration functions to config.rs. Remove lib.rs

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,23 @@
-/// Functions 
+/// Configuration Functions 
+
+#[macro_use]
+extern crate serde_derive;
+extern crate toml;
+
+use std::path::Path;
+use toml::Value;
+use std::fs;
+
+#[derive(Deserialize)]
+pub struct Config {
+    library: String
+}
+
+/// Read a toml configuration file.
+pub fn read_config_file() -> Result<String, Box<std::error::Error + 'static>> {
+    let config_file = fs::read_to_string("library.toml")?;
+    Ok(config_file)
+}
 
 /// Set configuration options.
 fn config() -> {
